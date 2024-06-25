1fbeaf7a86c35cf42d5c0f4bc297fb06475f0d82
Cristian <cristiangarci10@gmail.com>
2024-06-25 16:50:24 +0200

    Initial Commit

---
+++ listNameApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  listNameApp
+//
+//  Created by Cristian Garcia on 25/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ listNameApp/listNameAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  listNameAppApp.swift
+//  listNameApp
+//
+//  Created by Cristian Garcia on 25/6/24.
+//
+
+import SwiftUI
+
+@main
+struct listNameAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
